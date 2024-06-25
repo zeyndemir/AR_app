bdc45980cccdcfa079742c6c57cde4a08bb02192
zeyndemir <zeynepdemir23@outlook.com>
2024-06-25 19:39:13 +0300

    new

---
+++ Assets/Sahne_degis.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Sahne_degis : MonoBehaviour
+{
+
+    public void Main_Animals_World()
+
+    {
+
+        SceneManager.LoadScene("Animals_World");
+
+    }
+
+    public void Main_About_Us()
+    {
+        SceneManager.LoadScene("About_Us");
+
+    }
+
+    public void Animals_World_Main()
+    {
+
+        SceneManager.LoadScene("Main");
+
+    }
+
+
+    public void About_Us_Main()
+    {
+
+        SceneManager.LoadScene("Main");
+
+    }
+
+
+    public void Dans_Fare_Animals_World()
+    {
+
+        SceneManager.LoadScene("Animals_World");
+
+    }
+
+    public void Animals_World_Tigerr()
+    {
+
+        SceneManager.LoadScene("Tigerr");
+
+    }
+
+    public void Tigerr_Animals_World()
+    {
+
+        SceneManager.LoadScene("Animals_World");
+
+    }
+
+
+    public void Animals_World_Dans_Fare()
+    {
+
+        SceneManager.LoadScene("Dans_Fare");
+
+    }
+
+    public void Animals_World_Cat()
+    {
+
+        SceneManager.LoadScene("Cat");
+
+
+    }
+
+    public void Cat_Animals_World()
+    {
+
+        SceneManager.LoadScene("Animals_World");
+
+    }
+
+    public void Tigerr_Tiger_Walk()
+    {
+
+        SceneManager.LoadScene("Tiger_Walk");
+
+    }
+
+    public void Tigerr_Tiger_Run()
+    {
+
+        SceneManager.LoadScene("Tiger_Run");
+
+    }
+    public void Tigerr_Tiger_Idle()
+    {
+
+        SceneManager.LoadScene("Tiger_Idle");
+
+    }
+
+    public void Dans_Fare_Dans_Fare_Laugh()
+    {
+        SceneManager.LoadScene("Dans_Fare_Laugh");
+    }
+
+    public void Dans_Fare_Dans_Fare_Hiphop()
+    {
+        SceneManager.LoadScene("Dans_Fare_Hiphop");
+    }
+
+
+    public void Cat_Cat_Jump()
+    {
+        SceneManager.LoadScene("Cat_Jump");
+    }
+
+    public void Cat_Cat_Idle()
+    {
+        SceneManager.LoadScene("Cat_Idle");
+    }
+
+    public void Cat_Cat_Walk()
+    {
+        SceneManager.LoadScene("Cat_Walk");
+    }
+
+    public void Project_Exit()
+    {
+
+        Application.Quit();
+    }
+
+}
